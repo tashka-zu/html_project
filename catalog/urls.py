c821589ba31670dadf4a09feb39dfce16f1a761a
«tashka-zu» <zubkovanatska21@gmail.com>
2025-11-05 11:11:10 +0300

    Добавлены страницы товаров, контактов и базовый шаблон

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),          # Главная страница
-    path('contacts/', views.contacts, name='contacts'),  # Страница контактов
+    path('product/<int:pk>/', views.product_detail, name='product_detail'),
+    path('', views.home, name='home'),
+    path('contacts/', views.contacts, name='contacts'),
 ]
