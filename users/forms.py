fd12dfe19722503332d557d1dcefdb65ad98bc8c
«tashka-zu» <zubkovanatska21@gmail.com>
2025-12-04 11:10:23 +0300

    Обновлена модель пользователя, формы и представления для регистрации и авторизации

--- users/forms.py
+++ users/forms.py
@@ -3,18 +3,13 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-
     class Meta:
         model = CustomUser
         fields = ('email', 'password1', 'password2')
 
-    def save(self, commit=True):
-        user = super().save(commit=False)
-        user.email = self.cleaned_data['email']
-        if commit:
-            user.save()
-        return user
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['email'].label = "Email"
 
 
 class CustomAuthenticationForm(AuthenticationForm):
--- users/models.py
+++ users/models.py
@@ -3,17 +3,19 @@ from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
 
 class CustomUser(AbstractUser):
-    email = models.EmailField(_('email address'), unique=True)
-    avatar = models.ImageField(upload_to='avatars/', verbose_name='Аватар', blank=True, null=True)
-    phone_number = models.CharField(max_length=20, verbose_name='Номер телефона', blank=True, null=True)
-    country = models.CharField(max_length=100, verbose_name='Страна', blank=True, null=True)
+    username = None
+    email = models.EmailField(_("email address"), unique=True)
+    avatar = models.ImageField(upload_to="avatars/", verbose_name="Аватар", blank=True, null=True)
+    phone_number = models.CharField(max_length=20, verbose_name="Номер телефона", blank=True, null=True)
+    country = models.CharField(max_length=100, verbose_name="Страна", blank=True, null=True)
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
 
     class Meta:
-        verbose_name = 'Пользователь'
-        verbose_name_plural = 'Пользователи'
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
 
     def __str__(self):
         return self.email
+
